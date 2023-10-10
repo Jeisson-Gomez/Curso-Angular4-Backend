93e6007f3ca347b862290eb187b21f75aa577c8d
Jeisson Gomez <71471533+jeypitbull@users.noreply.github.com>
2023-10-10 14:01:23 -0500

    Actualizacion del proyecto

--- bbdd.sql
+++ bbdd.sql
@@ -2,10 +2,10 @@ CREATE DATABASE IF NOT EXISTS Curso_Angular4;
 USE Curso_Angular4;
 
 CREATE TABLE productos(
-id			int(255) auto_increment not null,
-nombre 		varchar(255),
-description text,
-precio		varchar(255),
-imagen		varchar(255),
-CONSTRAINT pk_productos PRIMARY KEY(id)
+    id			int(255) auto_increment not null,
+    nombre 		varchar(255),
+    description text,
+    precio		varchar(255),
+    imagen		varchar(255),
+    CONSTRAINT pk_productos PRIMARY KEY(id)
 )ENGINE=InnoDb;
