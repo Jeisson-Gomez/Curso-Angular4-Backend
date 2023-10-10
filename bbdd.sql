7675b78a838a321d7fcc2d5ec5792a6e5ccdc853
Jeisson Gomez <71471533+jeypitbull@users.noreply.github.com>
2023-10-10 13:59:16 -0500

    Creacion de base de datos

---
+++ bbdd.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS Curso_Angular4;
+USE Curso_Angular4;
+
+CREATE TABLE productos(
+id			int(255) auto_increment not null,
+nombre 		varchar(255),
+description text,
+precio		varchar(255),
+imagen		varchar(255),
+CONSTRAINT pk_productos PRIMARY KEY(id)
+)ENGINE=InnoDb;
